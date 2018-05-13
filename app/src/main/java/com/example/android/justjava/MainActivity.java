315407032fe9c3cf914229db5e8f7d8406a171a4
Marcin Dolegowski <marcindolegowski84@gmail.com>
2018-05-13 20:28:47 +0200

    With live calculations and summary email intent after submit order.

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -10,13 +10,11 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-// import java.text.NumberFormat; <- unused, unused method below
 
 public class MainActivity extends AppCompatActivity {
 
 
     int quantity = 1; // declaring the quantity of coffee
-
 
 
     @Override
@@ -44,20 +42,29 @@ Method called when the Order button is clicked
         CheckBox chocolateCheckBox = (CheckBox) findViewById(R.id.chocolate_check_box);
         boolean addChocolate = chocolateCheckBox.isChecked();
 
-        int price = calculatePrice(creamCheckBoxState,addChocolate);
+        int price = calculatePrice(creamCheckBoxState, addChocolate);
+        String priceMessage = createOrderSummary(price, creamCheckBoxState, addChocolate, SelectedName);
 
-        displayMessage(createOrderSummary(price, creamCheckBoxState, addChocolate, SelectedName), SelectedName );
-        
-    
-         }
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:address@justjava.com")); // only email apps should handle this
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Just java order for " + SelectedName);
+        intent.putExtra(Intent.EXTRA_TEXT, priceMessage);
+
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+
+
+    }
+
     /**
-     *Method creates a summary of the order information and returns the text summary
-     * @param SelectedName of the customer
-     * @param addChocolate or not?
-     * @param creamCheckBoxState with cream or not?
-     * @param price calculated total price
-     * @return text summary of the order
+     * Method creates a summary of the order information and returns the text summary
      *
+     * @param SelectedName       of the customer
+     * @param addChocolate       or not?
+     * @param creamCheckBoxState with cream or not?
+     * @param price              calculated total price
+     * @return text summary of the order
      */
 
     private String createOrderSummary(int price, boolean creamCheckBoxState, boolean addChocolate, String SelectedName) {
@@ -74,8 +81,9 @@ Method called when the Order button is clicked
 
     /**
      * Method calculates the price of the order based on quantity
+     *
      * @param creamCheckBoxState with cream or not?
-     * @param addChocolate or not?
+     * @param addChocolate       or not?
      **/
 
 
@@ -83,12 +91,12 @@ Method called when the Order button is clicked
 
         int basePrice = 5;
 
-        if(creamCheckBoxState) {
+        if (creamCheckBoxState) {
 
             basePrice = basePrice + 1;
         }
 
-        if(addChocolate) {
+        if (addChocolate) {
 
             basePrice = basePrice + 2;
         }
@@ -115,14 +123,12 @@ Method called when the + button is clicked
                     "You can not order more than 100 coffees", Toast.LENGTH_SHORT).show();
 
             return;
-        }
-
-        else {
+        } else {
 
             quantity = quantity + 1;
 
             displayQuantity(quantity);
-            displayPrice();
+
         }
 
     }
@@ -140,18 +146,14 @@ Method called when the - button is clicked
                     "You can not order less than 1 coffee", Toast.LENGTH_SHORT).show();
 
             return;
-        }
-
-        else {
+        } else {
 
             quantity = quantity - 1;
 
             displayQuantity(quantity);
-            displayPrice();
 
 
         }
-
 
 
     }
@@ -163,56 +165,9 @@ Method displays given quantity value on the screen
     private void displayQuantity(int number) {
 
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
-        quantityTextView.setText(""+ number);
+        quantityTextView.setText("" + number);
 
     }
-
-
- /*
-Method displays current price on the screen
-*/              
-
-
-private void displayPrice() {
-
-    CheckBox creamCheckBox = (CheckBox) findViewById(R.id.whipped_cream_check_box);
-    boolean creamCheckBoxState = creamCheckBox.isChecked();
-
-    CheckBox chocolateCheckBox = (CheckBox) findViewById(R.id.chocolate_check_box);
-    boolean addChocolate = chocolateCheckBox.isChecked();
-
-    int price = calculatePrice(creamCheckBoxState,addChocolate);
-
-TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-priceTextView.setText("Total: $" + price);
-
-}
-
-
-
-
-/*
-Method displays price on the screen
-*/
-
-   private void displayMessage(String message, String SelectedName) {
-
-
-
-           Intent intent = new Intent(Intent.ACTION_SENDTO);
-           intent.setData(Uri.parse("mailto:address@justjava.com")); // only email apps should handle this
-           intent.putExtra(Intent.EXTRA_SUBJECT, "Just java order for " + SelectedName);
-           intent.putExtra(Intent.EXTRA_TEXT, message);
-
-           if (intent.resolveActivity(getPackageManager()) != null) {
-               startActivity(intent);
-           }
-
-
-
-
-    }
-
 
 
 }
